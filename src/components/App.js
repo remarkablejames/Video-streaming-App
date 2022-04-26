0216ac4727095d48b9576df202e01450cf5ec050
remarkablejames <lascasasjames85@gmail.com>
2022-04-26 13:02:21 +0200

    Initial setup of the application

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
