5a29ff841cde37c0869ac6f8ad0e4ac4e207a0ab
remarkablejames <lascasasjames85@gmail.com>
2022-04-26 14:22:45 +0200

    added routing functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1> App</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h2>Hello world</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<StreamList />} />
+          <Route path="/streams/new" exact element={<StreamCreate />} />
+          <Route path="/streams/edit" exact element={<StreamEdit />} />
+          <Route path="/streams/delete" exact element={<StreamDelete />} />
+          <Route path="/streams/show" exact element={<StreamShow />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
