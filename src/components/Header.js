12487b37b0937a790e0567e9526b78601760c168
remarkablejames <lascasasjames85@gmail.com>
2022-05-01 17:11:08 +0200

    edited App name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        Streamer
+        Stream App
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
