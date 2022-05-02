4cd57360f11d52416aaafa5eb4447660633a8899
remarkablejames <lascasasjames85@gmail.com>
2022-05-02 21:13:24 +0200

    added redux form to the application

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./components/App";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
