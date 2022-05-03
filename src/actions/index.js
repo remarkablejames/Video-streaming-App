034e31ddb924f15e0194cbe111e616b3fef48b6f
remarkablejames <lascasasjames85@gmail.com>
2022-05-03 06:45:41 +0200

    added straems reducer function

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,4 +15,25 @@ export const signOut = () => {
 
 export const createStream = (formValues) => async (dispatch) => {
   const response = await streamsAPI.post("/streams", formValues);
+  dispatch({ type: "CREATE_STREAM", payload: response.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streamsAPI.get("/streams");
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streamsAPI.get(`streams/ ${id}`);
+  dispatch({ type: "FETCH_STREAM", payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streamsAPI.put(`/streams/${id}`, formValues);
+  dispatch({ type: "EDIT_STREAM", payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streamsAPI.get(`/streams/${id}`);
+  dispatch({ type: "DELETE_STREAMS", payload: id });
 };
